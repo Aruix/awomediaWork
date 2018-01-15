f4f80ec6f9ce1e795317d1c5c50713b057082b9a
Aruix <943903528@qq.com>
2018-01-15 17:08:05 +0800

    添加了评论列表和活动，新建八一的项目

---
+++ bayi/js/rem.js
@@ -0,0 +1,20 @@
+// 设置rem数值
+(function (doc, win) {
+  var docEl = doc.documentElement,
+	resizeEvt = 'orientationchange' in window ? 'orientationchange' : 'resize',
+	recalc = function () {
+	  var clientWidth = docEl.clientWidth;
+	  if (!clientWidth) return;
+	  /*
+	  if(clientWidth>700){
+			docEl.style.fontSize = '100px';
+			return;
+	  };
+	  */
+	  docEl.style.fontSize = 100 * (clientWidth / 1646) + 'px';
+	};
+
+  if (!doc.addEventListener) return;
+  win.addEventListener(resizeEvt, recalc, false);
+  doc.addEventListener('DOMContentLoaded', recalc, false);
+})(document, window);
